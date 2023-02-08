e30e91a1748559f40b3f051137b815cb536e6a19
Andrew-Lk <100060030+Andrew-Lk@users.noreply.github.com>
2023-02-08 22:02:17 +0700

    StringBuilder -> String

--- src/Main.java
+++ src/Main.java
@@ -169,11 +169,13 @@ public class Main {
     private static String transform_number_to_roman_numeral(int number) {
         int[] roman_value_list = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] roman_char_list = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < roman_value_list.length; i += 1) {
+        //StringBuilder res = new StringBuilder();
+        String res = "";
+        for (int i = 0; i < roman_value_list.length; i++) {
             while (number >= roman_value_list[i]){
                 number -= roman_value_list[i];
-                res.append(roman_char_list[i]);
+                //res.append(roman_char_list[i]);
+                res += roman_char_list[i];
             }
         }
         return res.toString();
